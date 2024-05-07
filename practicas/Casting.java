1dff1494e1a756c6475a04df48d8fe0014e29b76
Andrea Itzel Avendano Lima <itzelima69@gmail.com>
2024-05-06 18:00:09 -0600

    Calculadora de andy

---
+++ practicas/Casting.java
@@ -0,0 +1,14 @@
+package practicas;
+
+public class Casting {
+    public static void main(String[] args) {
+        //cambiar el tipo de dao a una variable
+
+        int numeroEntero = 22;
+        double numeroDouble = numeroEntero;
+
+        //imprimir los dos valores para verificar
+        System.out.println("Valor entero: " + numeroEntero);
+        System.out.println("Valor double: " + numeroDouble);
+    }
+}
